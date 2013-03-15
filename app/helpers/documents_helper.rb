a90fed18afcd616064a326099571e3ba1a63d3bf
Ilya Anisimoff <ilya@intersect.org.au>
2013-03-15 15:57:50 +1100

    Updates python cut'n'paste code

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,8 +1,8 @@
 module DocumentsHelper
 
 	def get_python_query
-		python_string = "import hcsvlab\nurl = 'http://gsw1-hcsvlab-test3-vm.intersect.org.au/documents.json'\n"
-		python_string << "query = hcsvlab.query.Query(url)\n"
+		python_string = "from hcsvlab.query import Query\nurl = 'http://gsw1-hcsvlab-test3-vm.intersect.org.au/documents.json'\n"
+		python_string << "query = Query(url)\n"
 		query_fields = %w[corpus_name sub_corpus_name year_from year_to media_type]
 		query_hash = params.select { |k, v| query_fields.include? k unless v.empty? }
 		python_string << "params = " + query_hash.to_s.gsub('=>', ': ') + "\n"
