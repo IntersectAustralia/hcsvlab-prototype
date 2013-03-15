6048b7094d7804a47fc998f5122bc39c07fa2227
Ilya Anisimoff <ilya@intersect.org.au>
2013-03-15 14:32:31 +1100

    Adds python cut'n'paste code

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,12 @@
 module DocumentsHelper
+
+	def get_python_query
+		python_string = "import hcsvlab\nurl = 'http://gsw1-hcsvlab-test3-vm.intersect.org.au/documents.json'\n"
+		python_string << "query = hcsvlab.query.Query(url)\n"
+		query_fields = %w[corpus_name sub_corpus_name year_from year_to media_type]
+		query_hash = params.select { |k, v| query_fields.include? k unless v.empty? }
+		python_string << "params = " + query_hash.to_s.gsub('=>', ': ') + "\n"
+		python_string << "query.query(params)\n"
+		python_string << "query.download('my_corpus')\t# change 'my_corpus' to the name of the directory you want to download to\n"
+	end
 end
