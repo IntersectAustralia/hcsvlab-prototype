35e4e201e7a2d1a4745d239a8404c276a0b97ae5
Chris Kenward <chrisk@intersect.org.au>
2013-03-19 16:12:21 +1100

    Refactor hard coded hostname and port

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,7 +1,7 @@
 module DocumentsHelper
 
 	def get_python_query
-		python_string = "from hcsvlab.query import Query\nurl = 'http://gsw1-hcsvlab-test3-vm.intersect.org.au:8080/documents.json'\n"
+		python_string = "from hcsvlab.query import Query\nurl = 'http://" + request.host_with_port + "/documents.json'\n"
 		python_string << "query = Query(url)\n"
 		query_fields = %w[corpus_name sub_corpus_name year_from year_to media_type]
 		query_hash = params.select { |k, v| query_fields.include? k unless v.empty? }
@@ -12,7 +12,7 @@ module DocumentsHelper
 
   def get_rstudio_query
     python_string = "library(\"hcsvlab\")\n"
-    python_string << "url = \"http://gsw1-hcsvlab-test3-vm.intersect.org.au:8080/documents.json\"\n"
+    python_string << "url = \"http://" + request.host_with_port + "/documents.json\"\n"
     python_string << "corpus_dir = \"my_corpus\" # change 'my_corpus' to the name of the directory you want to download to\n"
     query_fields = %w[corpus_name sub_corpus_name year_from year_to media_type]
     query_hash = params.select { |k, v| query_fields.include? k unless v.empty? }
